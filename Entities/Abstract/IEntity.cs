9d2f2d1e81f006a50760ddd992a3c18bbefe7278
SeyfettinKaratas <skaratas@outlook.com.tr>
2021-02-21 00:26:49 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity alanlar bu classın veri tabanıdır.
+    public interface IEntity
+    {
+    }
+}
